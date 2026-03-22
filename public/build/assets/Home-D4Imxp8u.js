96fbb5437c8b0a97bc506e5c8aa086694b783c2a
catur <catur@fisip.net>
2026-03-22 12:22:57 +0700

    feat: bulid

---
+++ public/build/assets/Home-D4Imxp8u.js
@@ -0,0 +1,1 @@
+import{j as e,H as t}from"./app-DcnpCu6v.js";function s(){return e.jsxs(e.Fragment,{children:[e.jsx(t,{title:"Home"}),e.jsx("div",{children:e.jsx("h1",{children:"Home"})})]})}export{s as default};
