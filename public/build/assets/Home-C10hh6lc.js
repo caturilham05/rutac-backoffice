f6bc96c08c68fbe85ab77dd34a5d4305d47e231e
catur <catur@fisip.net>
2026-06-03 12:21:31 +0700

    feat: build

---
+++ public/build/assets/Home-C10hh6lc.js
@@ -0,0 +1,1 @@
+import{j as e,H as t}from"./app-B9r8oXiH.js";function s(){return e.jsxs(e.Fragment,{children:[e.jsx(t,{title:"Home"}),e.jsx("div",{children:e.jsx("h1",{children:"Home"})})]})}export{s as default};
