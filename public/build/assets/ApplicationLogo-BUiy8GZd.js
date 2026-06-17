972e1d1e9ac13bd2cfcf69ed6376f9621e00e55d
catur <catur@fisip.net>
2026-06-17 18:30:57 +0700

    build: products

--- public/build/assets/ApplicationLogo-BUiy8GZd.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-C6-E5oNu.js";function e(o){return r.jsx(r.Fragment,{children:r.jsx("h1",{children:"Rutac Perfume"})})}export{e as A};
--- public/build/assets/Products-Ia5mh4EY.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-C6-E5oNu.js";function o(){return r.jsx("div",{children:"Products"})}export{o as default};
