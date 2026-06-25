3751844c24cd6214d7636ed2728398799b916bf2
catur <catur@fisip.net>
2026-06-26 00:55:01 +0700

    fix: unsigend

--- database/migrations/2026_06_25_215720_add_field_purchase_products.php
+++ database/migrations/2026_06_25_215720_add_field_purchase_products.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('purchase_products', function (Blueprint $table) {
             $table->bigInteger('purchase_id');
             $table->index('purchase_id');
-            $table->foreign('purchase_id')->references('id')->on('purchases')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('purchase_id')->unsigned()->references('id')->on('purchases')->onUpdate('cascade')->onDelete('cascade');
         });
     }
 
