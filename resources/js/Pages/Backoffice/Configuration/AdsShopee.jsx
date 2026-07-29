af63535419c88652952ebe56f85318d7982c7b29
catur ilham juniarto <caturilham05@gmail.com>
2026-07-30 00:01:54 +0700

    feat: action pause or resume ads

--- resources/js/Pages/Backoffice/Configuration/AdsShopee.jsx
+++ resources/js/Pages/Backoffice/Configuration/AdsShopee.jsx
@@ -1,6 +1,7 @@
 import DataTable from '@/Components/DataTable';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import { Head, usePage } from '@inertiajs/react';
+import { Head, usePage, Link } from '@inertiajs/react';
+import { Pause, Play } from 'lucide-react';
 
 function AdsShopee() {
     const { ads } = usePage().props;
@@ -13,7 +14,31 @@ function AdsShopee() {
         { key: 'campaign_budget', label: 'Budget' },
         { key: 'start_time', label: 'Start Time' },
         { key: 'end_time', label: 'End Time' },
+        { key: 'roas_target', label: 'Roas Target' },
+        {
+            key: 'edit',
+            label: 'Action',
+            render: (row) => (
+                <div className="flex gap-2">
+                    <button
+                        onClick={() => handleAds(row.id)}
+                        className={`rounded px-3 py-1 text-white ${row.status === 'ongoing' ? 'bg-red-500' : 'bg-green-500'}`}
+                    >
+                        {statusIcon[row.status] ?? null}
+                    </button>
+                </div>
+            )
+        }
     ];
+
+    const statusIcon = {
+        ongoing: <Pause size = {15} />,
+        paused : <Play size = {15} />
+    }
+
+    const handleAds = () => {
+        console.log('first')
+    }
 
     return (
         <AuthenticatedLayout
