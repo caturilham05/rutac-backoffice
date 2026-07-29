9d72e1e7b740fb97efbd88a53918c683986c2793
catur ilham juniarto <caturilham05@gmail.com>
2026-07-30 01:16:13 +0700

    build

---
+++ public/build/assets/AdsShopee-DABX9UIl.js
@@ -0,0 +1,11 @@
+import{a as c,j as e,H as p,r as u}from"./app-CaEIsI3r.js";import{D as g}from"./DataTable-DJbicnU-.js";import{c as n,A as m}from"./AuthenticatedLayout-D0pBchvn.js";import"./ApplicationLogo-BkvI8BhK.js";import"./transition-DPuGY7Le.js";/**
+ * @license lucide-react v1.21.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=[["rect",{x:"14",y:"3",width:"5",height:"18",rx:"1",key:"kaeet6"}],["rect",{x:"5",y:"3",width:"5",height:"18",rx:"1",key:"1wsw3u"}]],x=n("pause",h);/**
+ * @license lucide-react v1.21.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=[["path",{d:"M5 5a2 2 0 0 1 3.008-1.728l11.997 6.998a2 2 0 0 1 .003 3.458l-12 7A2 2 0 0 1 5 19z",key:"10ikf1"}]],b=n("play",y);function A(){const{ads:s,flash:t}=c().props;console.log(t);const o=[{key:"name",label:"Campaign Name"},{key:"type",label:"Type"},{key:"status",label:"Status"},{key:"bidding_method",label:"Bidding Method"},{key:"campaign_budget",label:"Budget"},{key:"start_time",label:"Start Time"},{key:"end_time",label:"End Time"},{key:"roas_target",label:"Roas Target"},{key:"edit",label:"Action",render:a=>d[a.status]&&e.jsx("div",{className:"flex gap-2",children:e.jsx("button",{onClick:()=>{r(a.marketplace_id,a.campaign_id,a.status)},className:`rounded px-3 py-1 text-white ${a.status==="ongoing"?"bg-red-500":"bg-green-500"}`,children:d[a.status]})})}],d={ongoing:e.jsx(x,{size:15}),paused:e.jsx(b,{size:15})},r=(a,l,i)=>{u.post(route("shopee.ads.edit",a),{campaign_id:l,edit_action:i==="ongoing"?"pause":i==="paused"?"resume":"",preserveScroll:!1})};return e.jsxs(m,{header:e.jsx("h2",{className:"text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200",children:"Shopee Ads Configuration"}),children:[e.jsx(p,{title:"Shopee Ads"}),t.success&&e.jsx("div",{className:"mb-4 mt-4 rounded bg-green-300 p-4 text-green-700",children:t.success}),t.error&&e.jsx("div",{className:"mb-4 mt-4 rounded bg-red-300 p-4 text-red-700",children:t.error}),e.jsx("div",{className:"p-6",children:e.jsx(g,{columns:o,data:s.data,pagination:s,baseUrl:route("shopee.ads.index")})})]})}export{A as default};
