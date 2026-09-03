1cac8fd92a69223998dfd3615650b0337411bcce
catur ilham juniarto <caturilham05@gmail.com>
2026-09-04 02:42:46 +0700

    fix

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\ShopeeWebhookController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/shopee/webhook', [ShopeeWebhookController::class, 'handle'])->name('shopee.webhook');
+// Route::post('/shopee/webhook', [ShopeeWebhookController::class, 'handle'])->name('shopee.webhook');
